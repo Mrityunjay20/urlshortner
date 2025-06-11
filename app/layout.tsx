339f66d72e62523c02f54965d84da8c7c2075358
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2025-06-12 01:04:23 +0530

    landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'mjxsh url shortner',
+  title: 'url shortner',
   description: 'Created by mjxsh',
   generator: 'v0.dev',
 }
